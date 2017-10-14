570d25bd394be751dc318477d9fdc37ca76393f0
Liujie <1002094787@qq.com>
2017-10-14 16:23:28 +0800

    秒钱  第四天

--- js/huoqi.js
+++ js/huoqi.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('.content-tab span').click(function() {
-		console.log($(this).eq($(this).index()))
 		$(this).addClass('active').siblings().removeClass('active');
+		$('.tab-con-items').eq($(this).index()).addClass('active').siblings().removeClass('active');
 	});
 });
